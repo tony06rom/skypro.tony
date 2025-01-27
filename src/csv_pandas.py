15249a90b9031b5487d503b5b5c44cdcc2d6a3e5
Tony <toshik06rom@rambler.ru>
2025-01-28 00:53:59 +0300

    finished func csv_pandas.py

--- src/csv_pandas.py
+++ src/csv_pandas.py
@@ -1,23 +1,60 @@
 import csv
 import pandas as pd
+import logging
 
 
-def csv_worker():
+logger = logging.getLogger("csv_pandas")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("../logs/csv_pandas.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(funcName)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+csv_transact = "../data/transactions.csv"
+excel_transact = "../data/transactions_excel.xlsx"
+TMP_PATH = "../tmp/"
+
+def csv_worker(include_file: str=csv_transact) -> list[dict[str, int | float | str]] | Exception | str:
     """ Считывает финансовые операции из CSV """
-    pass
+
+    logger.info(f"Старт программы 'csv_worker'")
+
+    try:
+        with open(include_file, mode='r', encoding="utf-8") as file:
+
+            logger.info(f"File '{include_file}' was opened")
+
+            csv_data = csv.DictReader(file, delimiter=';')
+            transact_list = [row for row in csv_data]
+
+            logger.info(f"File '{include_file}' was success transform to dict")
+
+            return transact_list
+
+    except FileNotFoundError as exc_info:
+
+        logger.error(f"File '{include_file}' not found")
+
+        return f"Error: {type(exc_info).__name__}, Incorrect path to file or file name"
 
 
-def excel_worker():
+def excel_worker(include_file: str=excel_transact) -> list[dict[str, int | float | str]] | Exception | str:
     """ Считывает финансовые операции из Excel """
-    pass
 
+    logger.info(f"Старт программы 'excel_worker'")
 
-"""
-Реализована функция для считывания финансовых операций из CSV.
-Функция для считывания финансовых операций из CSV принимает путь к файлу CSV в качестве аргумента.
-Функция для считывания финансовых операций из CSV выдает список словарей с транзакциями.
+    try:
+        excel_data = pd.read_excel(include_file, sheet_name='Лист 1')
 
-Реализована функция для считывания финансовых операций из Excel.
-Функция для считывания финансовых операций из Excel принимает путь к файлу Excel в качестве аргумента.
-Функция для считывания финансовых операций из Excel выдает список словарей с транзакциями.
-"""
+        logger.info(f"File '{include_file}' was opened")
+
+        transact_list = excel_data.to_dict(orient='records')
+
+        logger.info(f"File '{include_file}' was success transform to dict")
+
+        return transact_list
+    except FileNotFoundError as exc_info:
+
+        logger.error(f"File '{include_file}' not found")
+
+        return f"Error: {type(exc_info).__name__}, Incorrect path to file or file name"
--- tests/test_csv_pandas.py
+++ tests/test_csv_pandas.py
@@ -1,3 +1,4 @@
 from src.csv_pandas import csv_worker, excel_worker
 import pytest
 from unittest.mock import mock_open, patch
+
